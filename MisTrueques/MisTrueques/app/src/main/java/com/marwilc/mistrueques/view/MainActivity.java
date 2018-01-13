06762db1cda12e2490805ff50146572e8338353b
Marwil Campos <marwilcampos@gmail.com>
2018-01-12 22:16:44 -0400

    add font roboto

--- MisTrueques/MisTrueques/app/src/main/java/com/marwilc/mistrueques/view/MainActivity.java
+++ MisTrueques/MisTrueques/app/src/main/java/com/marwilc/mistrueques/view/MainActivity.java
@@ -1,41 +1,67 @@
 package com.marwilc.mistrueques.view;
 
-import android.support.design.widget.NavigationView;
+import android.graphics.Typeface;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.View;
-import android.widget.ImageButton;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.marwilc.mistrueques.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;     // definicion de un drawer layout para hacer uso en side menu
+    private ActionBarDrawerToggle mToggle;  // se define un drawer toggle para poner un icono de menu en la barra de// acccion
+    private TextView textView;              // Se crearan textview para la prueba del Roboto font
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageButton imageButton = (ImageButton) findViewById(R.id.imgButtonMenu);
+        /* Colocacion de la fuente roboto a un textView
 
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                NavigationView navigationView = (NavigationView) findViewById(R.id.navigationView);
-                navigationView.showContextMenu();
-            }
-        });
-       // Toolbar toolbar = (Toolbar) findViewById(R.id.myActionBar);
+        textView = (TextView) findViewById(R.id.textViewMainLayout);
+        Typeface roboto = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Medium.ttf");
+        textView.setTypeface(roboto);
+        */
+        /* Toolbar custom
+         */
+        Toolbar toolbar = (Toolbar) findViewById(R.id.myActionBar);
 
-        //if (toolbar != null)
-          //  setSupportActionBar(toolbar);
+        if (toolbar != null)
+            setSupportActionBar(toolbar);
+
+        /* Se ubican los layouts respectivos con su id
+         */
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open, R.string.close);
+
+        /* Se coloca en modo escucha el toggle
+         */
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        /* Se activa el icono de menu en la barra de accion
+         */
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 
+    /**
+     * Este metodo indica si esta seleccionado el boton de de menu en la barra
+     * @param item
+     * @return boolean
+     */
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.navigation_menu,menu);
-        return true;
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
